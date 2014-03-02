73700d339f874fe40ea61aa723c303f05c891252
Eduardo Marin <edu@nevicaneser.(none)>
2014-03-02 15:46:36 +0100

    updated

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,7 +2,23 @@
 
 angular.module('dubidubaApp')
   .controller('MainCtrl', function ($scope, $http) {
+    
+  	$scope.items = [];
+
+
     $http.get('/api/item').success(function(items) {
       $scope.items = items;
+      var _i = 0;
+      for(_i = 0; _i < items.length; _i++){
+      	if(items[_i].photos[0]){
+    		$scope.items[_i].mainPhoto = "http://farm" 
+            + items[_i].photos[0].photo.farm 
+            + ".staticflickr.com/" 
+            + items[_i].photos[0].photo.server + "/" 
+            + items[_i].photos[0].photo.id + "_" 
+            + items[_i].photos[0].photo.secret + "_m.jpg";
+    	}
+      }
+      
     });
   });
---
+++ app/scripts/lib/angular-ui-tinymce/src/tinymce.js
@@ -0,0 +1,101 @@
+/**
+ * Binds a TinyMCE widget to <textarea> elements.
+ */
+angular.module('ui.tinymce', [])
+  .value('uiTinymceConfig',
+    {
+        selector: "textarea",
+        theme: "modern",
+        plugins: [
+            "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            "insertdatetime media nonbreaking save table contextmenu directionality",
+            "emoticons template paste textcolor"
+        ],
+        toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+        toolbar2: "print preview media | forecolor backcolor emoticons",
+        image_advtab: true,
+        templates: [
+            {title: 'Test template 1', content: 'Test 1'},
+            {title: 'Test template 2', content: 'Test 2'}
+        ]
+    }
+
+  )
+  .directive('uiTinymce', ['uiTinymceConfig', function (uiTinymceConfig) {
+    uiTinymceConfig = uiTinymceConfig || {};
+    var generatedIds = 0;
+    return {
+      //require: 'ngModel',
+      scope:{ externalData : "=", externalDataReady : "="},
+      link: function (scope, elm, attrs, ngModel) {
+        var expression, options, tinyInstance,
+          updateView = function () {
+            scope.externalData=elm.val();
+            if (!scope.$$phase) {
+              scope.$apply();
+            }
+          };
+        // generate an ID if not present
+        if (!attrs.id) {
+          attrs.$set('id', 'uiTinymce' + generatedIds++);
+        }
+
+        if (attrs.uiTinymce) {
+          expression = scope.$eval(attrs.uiTinymce);
+        } else {
+          expression = {};
+        }
+        options = {
+          // Update model when calling setContent (such as from the source editor popup)
+          setup: function (ed) {
+            var args;
+            ed.on('init', function(args) {
+              _render();
+            });
+            // Update model on button click
+            ed.on('ExecCommand', function (e) {
+              ed.save();
+              updateView();
+            });
+            // Update model on keypress
+            ed.on('KeyUp', function (e) {
+              ed.save();
+              updateView();
+            });
+            // Update model on change, i.e. copy/pasted text, plugins altering content
+            ed.on('SetContent', function (e) {
+              if(!e.initial){
+                ed.save();
+                updateView();
+              }
+            });
+            if (expression.setup) {
+              scope.$eval(expression.setup);
+              delete expression.setup;
+            }
+          },
+          mode: 'exact',
+          elements: attrs.id
+        };
+        // extend options with initial uiTinymceConfig and options from directive attribute value
+        angular.extend(options, uiTinymceConfig, expression);
+        setTimeout(function () {
+          tinymce.init(options);
+        });
+
+        scope.$watch('externalDataReady', function(data){
+          _render();
+        });
+
+        function _render() {
+          if (!tinyInstance) {
+            tinyInstance = tinymce.get(attrs.id);
+          }
+          if (tinyInstance) {
+            tinyInstance.setContent(scope.externalData || '');
+          }
+        };
+      }
+    };
+  }]);
