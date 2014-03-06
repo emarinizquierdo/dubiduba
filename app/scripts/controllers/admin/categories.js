bb2305568ea3bfd88b253d9fd3c78f0028de671b
Eduardo Marin <edu@nevicaneser.(none)>
2014-03-06 22:30:47 +0100

    categories added

--- app/scripts/controllers/admin/categories.js
+++ app/scripts/controllers/admin/categories.js
@@ -3,18 +3,22 @@
 angular.module('dubidubaApp')
   .controller('AdminCategoriesCtrl', function ($scope, $http, Category ) {
     
-    $scope.categories = [];
+    var _existCategories = false;
+
+    $scope.categories = {};
   	$scope.isCollapsed = true;  
 
-  	$scope.saveCategory = function(p_data){
+  	$scope.saveCategories = function(){
 
-        Category.create(p_data, _OnSuccess, _OnError); 
-        
+        if(_existCategories){
+            Category.update($scope.categories, _OnSuccess, _OnError); 
+        }else{
+            Category.create($scope.categories, _OnSuccess, _OnError); 
+        }
+
     }
 
     function _OnSuccess(){
-    	$scope.isCollapsed = true; 
-    	$scope.newCategory = {};
     	_LoadCategories();
     }
 
@@ -24,19 +28,50 @@ angular.module('dubidubaApp')
 
     function _LoadCategories(){
     	$http.get('/api/category').success(function(p_data) {
-        	$scope.categories = p_data;
+        	if(p_data[0]){
+                _existCategories = true;
+                $scope.categories = p_data[0];
+            }else{
+                $scope.categories.categories = [{
+                    id: new Date().getTime(),
+                    name: null,
+                    children: []
+                }];
+            }
         }).error(function(error){
             
         });
     }
 
-    $scope.RemoveCategory = function( p_id ){
-    	$http.delete('/api/category/' + p_id).success(function(p_data) {
-        	_LoadCategories();
-        }).error(function(error){
-            
+    $scope.toggle = function(scope) {
+      scope.collapsed = !scope.collapsed;
+    };
+
+    $scope.newSubCategory = function( scope ) {
+        var categoryItem = scope.category;
+        categoryItem.children.push({
+            id: new Date().getTime(),
+            name: null,
+            children: []
         });
-    }
+    };
+
+    $scope.newCategory = function( scope ) {
+        $scope.categories.categories.push({
+            id: new Date().getTime(),
+            name: null,
+            children: []
+        });
+    };
+
+    $scope.removeCategories = function(scope) {
+      //scope.removeItem();
+      var index = scope.$index;
+      if (index > -1) {
+        scope.sortableModelValue.splice(index, 1)[0];
+      }
+      saveCategories();
+    };
 
     _LoadCategories();
 
