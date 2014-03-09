9484998e691fe5e44adb4e4d822729323913fd2b
Eduardo Marin <edu@nevicaneser.(none)>
2014-03-09 20:06:03 +0100

    categories and articles related

--- app/scripts/controllers/admin/article/edit.js
+++ app/scripts/controllers/admin/article/edit.js
@@ -4,7 +4,8 @@ angular.module('dubidubaApp')
 .controller('AdminArticleEditCtrl', ['$scope', '$http', '$routeParams', '$location', 'Item', function ($scope, $http, $routeParams, $location, Item) {
 	
 	$scope.errors = {};
-    $scope.articleData = {}
+    $scope.articleData = {};
+    $scope.categories = [];
     $scope.imagesLoading = 0;
     $scope.tinyMCE = false;
     
@@ -25,6 +26,7 @@ angular.module('dubidubaApp')
         $scope.imagesToLoad[$scope.imagesToLoad.length] = {loading : false};
         $scope.articleData.sizeSelected = $scope.sizes[$scope.articleData.sizeSelected.value];
         $scope.tinyMCE = true;
+        _LoadCategories();
     }
 
     $scope.goTo = function( p_route ){
@@ -43,6 +45,9 @@ angular.module('dubidubaApp')
             p_data.photos = $scope.imagesToLoad;
         }
 
+        p_data.category = $scope.categorySelected.id;
+        p_data.subcategory = $scope.subcategorySelected;
+
         if($routeParams.id){
             Item.update(p_data, _OnSuccess, _OnError);
         }else{
@@ -59,16 +64,37 @@ angular.module('dubidubaApp')
     	 $scope.errors.other = err.message;
     }
 
-    if($routeParams.id){
-        $http.get('/api/item/' + $routeParams.id).success(function(article) {
-        $scope.articleData = article;
-          _Init();
+    function _LoadItem(){
+        if($routeParams.id){
+            $http.get('/api/item/' + $routeParams.id).success(function(article) {
+            $scope.articleData = article;
+              _Init();
+            }).error(function(error){
+                
+            });
+        }else{
+            $scope.imagesToLoad =[];
+            $scope.imagesToLoad[0] = {loading : false};
+        }
+    } 
+
+    function _LoadCategories(){
+        $http.get('/api/category').success(function(p_data) {
+            if(p_data[0]){
+                $scope.categories = p_data[0].categories;
+                var _i = 0;
+                for(_i = 0; _i < $scope.categories.length; _i++){
+                    if($scope.articleData.category == $scope.categories[_i].id){
+                        $scope.categorySelected = $scope.categories[_i];
+                    }
+                }
+                $scope.subcategorySelected = $scope.articleData.subcategory;
+            }
         }).error(function(error){
             
         });
-    }else{
-        $scope.imagesToLoad =[];
-        $scope.imagesToLoad[0] = {loading : false};
     }
+
+    _LoadItem();
 
 }]);
